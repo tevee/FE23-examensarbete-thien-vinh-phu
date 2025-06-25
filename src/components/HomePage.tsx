d37586dc6d6fc1e328d124786d9ded70635ad3dd
tevee <brydiginte69@gmail.com>
2025-06-25 23:57:01 +0200

    Added material ui and axios node package

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,3 +1,10 @@
+import Button from "@mui/material/Button";
+
 export default function HomePage() {
-  return <div>HELLO WROLD I AM ALIOVE!!!</div>;
+  return (
+    <div>
+      HELLO WROLD I AM ALIOVE!!!
+      <Button variant="contained">MUI BUTTON</Button>
+    </div>
+  );
 }
